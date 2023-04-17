4a8eb4a9cc63f06ef5a8426e2f8de5b92865e7c7
Andrew Robles <andrewrobles@verizon.net>
2023-04-17 05:26:16 -0700

    wip

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:todo_id>/', views.detail, name='detail'),
 ]
