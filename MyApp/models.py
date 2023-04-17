6f2c32a43851511f4d3ed6b43e60258b76aa586a
Andrew Robles <andrewrobles@verizon.net>
2023-04-17 04:35:58 -0700

    wip

---
+++ MyApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .utils import strike
+
+class ToDoItem(models.Model):
+    text = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.done == True:
+            return strike(self.text)
+        else:
+            return self.text
