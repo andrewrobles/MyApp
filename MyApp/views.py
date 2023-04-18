b5f5d506f7463bdaa964890b576ee1e21380b9fa
Andrew Robles <andrewrobles@verizon.net>
2023-04-18 15:49:40 -0700

    wip

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render 
+
+from MyApp.models import ToDoItem
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    context = {
+        'todo_list': ToDoItem.objects.all()
+    }
+
+    return render(request, 'MyApp/index.html', context)
