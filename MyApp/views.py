bf3985ace353c9b1a17a914530a92b7659f57b6a
Andrew Robles <andrewrobles@verizon.net>
2023-04-25 17:23:28 -0700

    wip

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 
 from MyApp.models import ToDoItem
 
@@ -6,5 +6,16 @@ def index(request):
     context = {
         'todo_list': ToDoItem.objects.all()
     }
-
     return render(request, 'MyApp/index.html', context)
+
+def do(request, id):
+    todo = ToDoItem.objects.get(id=id)
+    todo.done = True
+    todo.save()
+    return redirect('/')
+
+def undo(request, id):
+    todo = ToDoItem.objects.get(id=id)
+    todo.done = False 
+    todo.save()
+    return redirect('/')
